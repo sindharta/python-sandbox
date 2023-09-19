3d21c947a24e984ad70575b67ee6672ee0b8afc5
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-09-19 15:55:51 +0900

    default

--- train-test-split-csv.py
+++ train-test-split-csv.py
@@ -20,7 +20,7 @@ parser = ArgumentParser()
 
 parser.add_argument('--percentage', '-p', required=False, default=0.2, help='The percentage of rows for test data (default: 0.2)')
 parser.add_argument('--input', '-i', required=True, help='The input CSV file')
-parser.add_argument('--has_header_row', '-r', required=False, default=True, help='Specifies if the CSV has a header row or not')
+parser.add_argument('--has_header_row', '-r', required=False, default=True, help='Specifies if the CSV has a header row or not (default: True)')
 parser.add_argument('--output_train', required=False, default="train.csv", help='The output train CSV file (default: train.csv)')
 parser.add_argument('--output_test', required=False, default="test.csv", help='The output test CSV file (default: test.csv)')
 
