32f0ff3c88591a6153073bce6d5fbbfe404647c8
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-10-02 14:24:26 +0900

    use lib

--- download-tf-dataset.py
+++ download-tf-dataset.py
@@ -7,19 +7,11 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
+from shinlib_csv import write_to_csv
+
 # [Note-sin: 2023-9-28]
 # if the programs fails to load "resource" when importing tensorflow_datasets on Windows, then we may have to apply this patch
 # https://github.com/tensorflow/datasets/commit/82215c7cf4b3e6df706a72c9b7ad8cede09f4d84
-
-def write_to_csv(outputFileName, dataList, header = ""):
-    with open(outputFileName, 'w', newline='', encoding='utf-8') as f:
-        if (len(header) > 0):
-            f.write(f'{header}')
-
-        writer = csv.writer(f)
-        for d in dataList:
-            writer.writerow(d)
-    print(f"Data written to {outputFileName}")
 
 def convert_tf_data_to_nparray(tf_data):
     sentences = []
