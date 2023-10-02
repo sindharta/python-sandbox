cf65bb39729d5ab5ec115a63e162cbfb5968abcb
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-10-02 14:24:17 +0900

    add library

---
+++ shinlib_csv.py
@@ -0,0 +1,11 @@
+
+def write_to_csv(outputFileName, dataList, header = ""):
+    with open(outputFileName, 'w', newline='', encoding='utf-8') as f:
+        if (len(header) > 0):
+            f.write(f'{header}')
+
+        writer = csv.writer(f)
+        for d in dataList:
+            writer.writerow(d)
+    print(f"Data written to {outputFileName}")
+
