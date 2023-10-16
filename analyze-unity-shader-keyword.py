6aa0e825ba232a712e6fd129447c1500ac60b9bc
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-10-16 20:44:14 +0900

    print definitions

--- analyze-unity-shader-keyword.py
+++ analyze-unity-shader-keyword.py
@@ -1,16 +1,11 @@
 from argparse import ArgumentParser
 import csv
 import os
-import random
-
-# This script assumes the input contains lines obtained by a grep script such as:
-# grep - r '#pragma\smulti_compile\|#pragma\sshader_feature'. > text.txt --include '*.shader'
-
-from shinmodule import write_to_csv
+import subprocess
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def isSpecialPragmaType(token):
+def is_special_pragma_type(token):
     if (token == "multi_compile"):
         return False
 
@@ -52,6 +47,21 @@ def write_keywords_dict_to_csv(outputFileName, keywords_dict):
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# Definition:
+# _SHADOWS_SOFT -> multi_compile -> A.hlsl -> [(line 10, actual_line), (line 20, actual_line)]
+#                                   B.hlsl -> [(line 90, actual_line), (line 80, actual_line)]
+# Usage:
+# _SHADOWS_SOFT -> A.hlsl -> [(line 10, actual_line), (line 20, actual_line)]
+#                  B.hlsl -> [(line 90, actual_line), (line 80, actual_line)]
+
+class ShaderKeyword:
+    def __init__(self, kw):
+        self.keyword = kw
+        self.declarations = {}
+        self.usages = {}
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 parser = ArgumentParser()
 
 parser.add_argument('--directory', '-d', required=True, help='The directory of the shader files')
@@ -72,14 +82,79 @@ if isError:
     exit()
 
 
-import os
-import subprocess
 
+# You can use -Hrn instead of -r if you want line numbers as well.
 proc = subprocess.run(["grep", "-r", "'#pragma\smulti_compile\|#pragma\sshader_feature'", input_dir, "--include", "'*.shader'"],
                       capture_output=True, text=True)
-grep_result=proc.stdout
+grep_result = proc.stdout
 lines = grep_result.splitlines()
+
+# Ex: multi_compile_fog -> A.hlsl -> [line 10, line 20]
+#                          B.hlsl -> [line 90, line 80]
+special_pragma_types = set()
+
+# Definition:
+# _SHADOWS_SOFT -> multi_compile -> A.hlsl -> [(line 10, actual_line), (line 20, actual_line)]
+#                                   B.hlsl -> [(line 90, actual_line), (line 80, actual_line)]
+# Usage:
+# _SHADOWS_SOFT -> A.hlsl -> [(line 10, actual_line), (line 20, actual_line)]
+#                  B.hlsl -> [(line 90, actual_line), (line 80, actual_line)]
+
+keywords_dict = {}
+
 for line in lines:
-    print(line)
+    tokens = line.split()
+
+    pragma_token = tokens[1]
+    if pragma_token.startswith('//'):
+        continue
+
+    pragma_type = tokens[2]
+
+    if is_special_pragma_type(pragma_type):
+        special_pragma_types.add(pragma_type)
+        continue
+
+    numTokens = len(tokens)
+
+    keyword_start_index = 3
+    keyword_tokens_in_line = " ".join(tokens[keyword_start_index:])
+    shader_file_path = tokens[0]
+    for index, keyword in enumerate(tokens[keyword_start_index:], keyword_start_index):
+        if keyword == "_" or keyword == "__":
+            continue
+
+        #            print(keyword, index, keyword_tokens_in_line)
+
+#        if not pragma_type in keywords_dict:
+#            keywords_dict[pragma_type] = dict()
+
+
+        if not keyword in keywords_dict:
+            keywords_dict[keyword] = ShaderKeyword(keyword)
+
+        # Usages
+        # _SHADOWS_SOFT -> A.hlsl -> [(line 10, actual_line), (line 20, actual_line)]
+        cur_shader_keyword = keywords_dict[keyword]
+        print(type(cur_shader_keyword.usages))
+        a = dict
+        print(type(a))
+
+
+        if not shader_file_path in cur_shader_keyword.usages:
+            cur_shader_keyword.usages[shader_file_path] = list()
+
+        # do another grep here
+        line_number = 10
+        cur_shader_keyword.usages[shader_file_path].append((line_number, "test"))
+
+
+for i, keyword in enumerate(keywords_dict):
+    print(keyword)
+
+    for j, shader_file_path in enumerate(keywords_dict[keyword].usages):
+        print("    ",shader_file_path)
+        for (usage_line, line_content) in keywords_dict[keyword].usages[shader_file_path]:
+            print("         ", usage_line, line_content)
 
 
