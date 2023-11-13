ac06dde8d4bcdb3d28ce14eed3e4355e57790e34
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-11-13 10:44:24 +0900

    add to structure

--- analyze-unity-shader-builtin-pragma.py
+++ analyze-unity-shader-builtin-pragma.py
@@ -293,7 +293,7 @@ special_pragma_types = set()
 # multi_compile_fog -> A.hlsl -> [(line 10, actual_line), (line 20, actual_line)]
 #                      B.hlsl -> [(line 90, actual_line), (line 80, actual_line)]
 
-keywords_dict = {}
+pragma_dict = {}
 keywords_grepped = set()
 
 for declaration_line_index, line in enumerate(lines):
@@ -314,9 +314,37 @@ for declaration_line_index, line in enumerate(lines):
     if not is_special_pragma_type(pragma_type):
         continue
 
+    #print(keyword, index, keyword_tokens_in_line)
+    if not pragma_type in pragma_dict:
+        pragma_dict[pragma_type] = ShaderKeyword(pragma_type)
 
-    print(pragma_type)
+    cur_shader_keyword = pragma_dict[pragma_type]
+
+
+
+    cur_shader_keyword.add_shader_usage(shader_file_path, declaration_line_number, usage_line_content)
+
+    print(shader_file_path, usage_line_content)
+
     continue
 
 
+
+
+
+
+# convert to list
+csv_list = []
+error_keywords = set()
+sorted_keywords = sorted(pragma_dict.keys())
+for pragma_type in sorted_keywords:
+    csv_list.append([pragma_type])
+    csv_list.extend(pragma_dict[pragma_type].to_string_list(start_col=1, source_url_root= args.source_url_root))
+
+header_row = [[f"Keywords (Total: {len(pragma_dict)})","","Type","Path", "LineNo", "LineContents"]]
+write_to_csv(args.output, csv_list, header_row)
+
+# print
+for line in csv_list:
+    print(line)
 
