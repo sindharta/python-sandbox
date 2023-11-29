b543f27b6a080e4fc17fc5567b59804d73c78d7c
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-11-29 15:31:55 +0900

    use shin_unity module

--- Unity/analyze-unity-shader-keyword.py
+++ Unity/analyze-unity-shader-keyword.py
@@ -7,34 +7,7 @@ import os
 import subprocess
 import re
 
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-def is_special_pragma_type(token):
-    if (token == "multi_compile"):
-        return False
-
-    if (token == "multi_compile_fragment"):
-        return False
-
-    if (token == "multi_compile_local_fragment"):
-        return False
-
-    if (token == "multi_compile_local"):
-        return False
-
-    if (token == "multi_compile_vertex"):
-        return False
-
-    if (token == "shader_feature"):
-        return False
-
-    if (token == "shader_feature_local"):
-        return False
-
-    if (token == "shader_feature_local_fragment"):
-        return False
-
-    return True
+from shin_unity import is_special_pragma_type
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
