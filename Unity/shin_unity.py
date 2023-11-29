56ed433764a4dac67b4cb40163929b5a1ab2181f
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-11-29 15:31:30 +0900

    add shin_unity module

---
+++ Unity/shin_unity.py
@@ -0,0 +1,31 @@
+
+def is_special_pragma_type(token):
+    if (token == "multi_compile"):
+        return False
+
+    if (token == "multi_compile_fragment"):
+        return False
+
+    if (token == "multi_compile_local_fragment"):
+        return False
+
+    if (token == "multi_compile_local"):
+        return False
+
+    if (token == "multi_compile_vertex"):
+        return False
+
+    if (token == "shader_feature"):
+        return False
+
+    if (token == "shader_feature_local"):
+        return False
+
+    if (token == "shader_feature_local_vertex"):
+        return False
+
+    if (token == "shader_feature_local_fragment"):
+        return False
+
+    return True
+
