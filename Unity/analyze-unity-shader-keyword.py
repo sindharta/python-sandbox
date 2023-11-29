8321f23617ffee460f06b9225f3ab8471a7813e8
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-11-29 15:37:10 +0900

    run_grep, read_file_all_lines

--- Unity/analyze-unity-shader-keyword.py
+++ Unity/analyze-unity-shader-keyword.py
@@ -7,40 +7,7 @@ import os
 import subprocess
 import re
 
-from shin_unity import is_special_pragma_type, write_to_csv
-
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# input_dir: can be a string or a list of strings
-# include_file_extensions: can contain multiple extensions, ex: "shader,hlsl"
-# An example of the called grep:
-#   grep -Hrn DEBUG_DISPLAY <package_path>/com.unity.render-pipelines.universal@15.0.6/Shaders/2D <package_path>/com.unity.render-pipelines.core@15.0.6 --include=*.{shader,hlsl,cginc,cg}
-
-def run_grep(input_dir, pattern, include_file_extensions, is_exact_match = False):
-
-    grep_options = "-Hrn"
-    if is_exact_match:
-        grep_options += "w"
-
-    # -Hrn with line numbers
-    if type(input_dir) == list:
-        proc = subprocess.run(["grep", grep_options, pattern, *input_dir, "--include=*.{" + include_file_extensions + "}"], capture_output=True, text=True)
-    else:
-        proc = subprocess.run(["grep", grep_options, pattern, input_dir, "--include=*.{" + include_file_extensions + "}"], capture_output=True, text=True)
-
-    grep_result = proc.stdout
-    if (grep_result):
-        return grep_result.splitlines()
-    else:
-        return []
-
-
-def read_file_all_lines(filePath):
-    ret = []
-    with open(filePath, encoding='utf-8') as f:
-        ret = f.readlines()
-
-    return ret
+from shin_unity import is_special_pragma_type, write_to_csv, read_file_all_lines, run_grep
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
